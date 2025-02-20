be3f9a243cd7210513bc7072fa13dc3d4f460f45
Matheus Marques <146294794+math-marques@users.noreply.github.com>
2025-02-20 14:13:55 -0300

    Update TipoAcao.cs

--- trilha-net-azure-desafio/Models/TipoAcao.cs
+++ trilha-net-azure-desafio/Models/TipoAcao.cs
@@ -1,5 +1,6 @@
 namespace TrilhaNetAzureDesafio.Models
 {
+//criação de um Enum só pra listar os tipos de ação que incializa e ajudar na reutilização posterior
     public enum TipoAcao
     {
         Inclusao,
